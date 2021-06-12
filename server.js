b0e5cfef480ffb70da79bacead70593ffe35fb9a
Una Ada <trewbot@gmail.com>
2021-06-11 23:58:06 -0700

    style: reformat server.js

--- server.js
+++ server.js
@@ -1,41 +1,36 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+/*----- Imports --------------------------------------------------------------*/
+const createError = require('http-errors'),
+  express = require('express'),
+  path = require('path'),
+  cookieParser = require('cookie-parser'),
+  logger = require('morgan'),
+  indexRouter = require('./routes/index'),
+  flightsRouter = require('./routes/flights'),
+  app = express();
 
-var indexRouter = require('./routes/index');
-var flightsRouter = require('./routes/flights');
-
-var app = express();
-
-// view engine setup
+/*----- Middleware -----------------------------------------------------------*/
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/*----- Routers --------------------------------------------------------------*/
 app.use('/', indexRouter);
 app.use('/flights', flightsRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+/*----- Error Handling -------------------------------------------------------*/
+app.use(function (req, res, next) {
   next(createError(404));
 });
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
+/*----- Exports --------------------------------------------------------------*/
 module.exports = app;

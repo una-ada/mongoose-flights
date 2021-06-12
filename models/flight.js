b2f3cf9448a569a7d2cbc05e766b2ab439ac6e21
Una Ada <trewbot@gmail.com>
2021-06-12 00:54:13 -0700

    feat(model): adds Flight model

---
+++ models/flight.js
@@ -0,0 +1,43 @@
+/**
+ * Flight model
+ * @author Una
+ * @version 2021.06.12
+ * @module models/flight
+ * @see module:routes/flights
+ */
+
+/*----- Imports --------------------------------------------------------------*/
+const mongoose = require('mongoose'),
+  /*----- Schema -------------------------------------------------------------*/
+  flightSchema = new mongoose.Schema(
+    {
+      airline: {
+        type: String,
+        enum: ['American', 'Delta', 'Southwest', 'United'],
+      },
+      airport: {
+        type: String,
+        enum: ['ATL', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN',
+      },
+      flightNo: {
+        type: Number,
+        required: true,
+        min: 10,
+        max: 9999,
+      },
+      departs: {
+        type: Date,
+        default: () =>
+          // Add a year to a date
+          (d => d.setFullYear(d.getFullYear() + 1))(
+            // Pass in the current date
+            new Date()
+          ),
+      },
+    },
+    { timestamps: true }
+  );
+
+/*----- Exports --------------------------------------------------------------*/
+module.exports = mongoose.model('Flight', flightSchema);

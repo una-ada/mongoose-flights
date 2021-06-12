c9bd0e722f70bef71537d115cae9ea42cfd46c13
Una Ada <trewbot@gmail.com>
2021-06-12 16:10:00 -0700

    fix(router): redirects / to /flights

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
 import { Router } from 'express';
 const router = Router();
-router.get('/', (req, res) => res.render('index', { title: 'Express' }));
+router.get('/', (req, res) => res.redirect('/flights'));
 export default router;

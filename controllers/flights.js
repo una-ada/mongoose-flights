7c4d834085b9b60673a4f29805a8d62ee92ec4f7
Una Ada <trewbot@gmail.com>
2021-06-12 12:52:51 -0700

    feat(ctrl): adds index controller

--- controllers/flights.js
+++ controllers/flights.js
@@ -11,7 +11,12 @@
 const Flight = require('../models/flight');
 
 /*----- Methods --------------------------------------------------------------*/
-const index = (req, res) => res.send('Test');
+const index = (req, res) =>
+  Flight.find({}, (err, flights) =>
+    err
+      ? console.error(err) || res.createError(500)
+      : console.log(flights) || res.send('Test') // change to res.render
+  );
 
 /*----- Exports --------------------------------------------------------------*/
 module.exports = {

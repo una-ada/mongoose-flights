7b1b5492fd9796ad6a4b894e1048e45dbb15cee4
Una Ada <trewbot@gmail.com>
2021-06-15 09:40:28 -0700

    feat(map): adds airport information

--- public/scripts/flight-map.js
+++ public/scripts/flight-map.js
@@ -25,4 +25,34 @@ class FlightMap {
         'contributors',
     }).addTo(this.map);
   }
+
+  /*----- Constants ----------------------------------------------------------*/
+  /** @const {Object} airports Information about airports. */
+  static airports = {
+    ATL: {
+      city: 'Atlanta, GA',
+      coords: [33.6367, -84.4281],
+      name: 'Hartsfield-Jackson Atlanta',
+    },
+    DFW: {
+      city: 'Dallas, TX',
+      coords: [32.8969, -97.0381],
+      name: 'Dallas/Fort Worth',
+    },
+    DEN: {
+      city: 'Denver, CO',
+      coords: [39.8617, -104.6731],
+      name: 'Denver',
+    },
+    LAX: {
+      city: 'Los Angeles, CA',
+      coords: [33.9425, -118.4081],
+      name: 'Los Angeles',
+    },
+    SAN: {
+      city: 'San Diego, CA',
+      coords: [32.7336, -117.1897],
+      name: 'San Diego',
+    },
+  };
 }
